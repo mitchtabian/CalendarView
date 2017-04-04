4938e5a07223dd15dcf2c7246fd19d57cc2072bf
Mitch Tabian <mitch@tabian.ca>
2017-04-04 10:07:27 -0700

    Source Code

---
+++ CalendarView/app/src/main/java/tabian/com/calendarview/CalendarActivity.java
@@ -0,0 +1,38 @@
+package tabian.com.calendarview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+/**
+ * Created by User on 4/4/2017.
+ */
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+                Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy: " + date);
+
+                Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ CalendarView/app/src/main/java/tabian/com/calendarview/MainActivity.java
@@ -0,0 +1,38 @@
+package tabian.com.calendarview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView theDate;
+    private Button btnGoCalendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        theDate = (TextView) findViewById(R.id.date);
+        btnGoCalendar = (Button) findViewById(R.id.btnGoCalendar);
+
+        Intent incomingIntent = getIntent();
+        String date = incomingIntent.getStringExtra("date");
+        theDate.setText(date);
+
+        btnGoCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
